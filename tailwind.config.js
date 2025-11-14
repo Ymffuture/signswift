7e9c944c540ebbc68f92886d02c1c9ff26646224
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-14 17:08:59 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-import colors from "tailwindcss/colors";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        gray: colors.gray, // restore gray scale including gray-50
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };

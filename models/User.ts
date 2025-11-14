2addb6beb10bc63d0609cd5da7c49012f60908d4
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-14 16:06:13 +0200

    Update User.ts

--- models/User.ts
+++ models/User.ts
@@ -1,20 +1,26 @@
 // models/User.ts
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Schema, Document, Model } from 'mongoose'
 
 export interface IUser extends Document {
-  name: string;
-  email: string;
-  passwordHash: string;
-  idNumber?: string;
-  idImageUrl?: string;
+  name: string
+  email: string
+  passwordHash: string
+  idNumber: string
+  idImageUrl: string
+  createdAt: Date
+  updatedAt: Date
 }
 
-const UserSchema = new mongoose.Schema<IUser>({
-  name: { type: String },
-  email: { type: String, unique: true, index: true },
-  passwordHash: { type: String },
-  idNumber: { type: String },
-  idImageUrl: { type: String },
-}, { timestamps: true });
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, index: true, required: true },
+    passwordHash: { type: String, required: true },
+    idNumber: { type: String, required: true },
+    idImageUrl: { type: String, required: true },
+  },
+  { timestamps: true }
+)
 
-export default (mongoose.models.User as mongoose.Model<IUser>) || mongoose.model<IUser>('User', UserSchema);
+const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>('User', UserSchema)
+export default User

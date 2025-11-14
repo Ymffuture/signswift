b8a4668309b774655d5f373f680f456f6777ce8e
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-14 18:09:50 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-// postcss.config.js
-export default {
+module.exports = {
   plugins: {
-    "@tailwindcss/postcss": {},   // ✔ THE ONLY VALID WAY
+    tailwindcss: {},
     autoprefixer: {},
   },
 };

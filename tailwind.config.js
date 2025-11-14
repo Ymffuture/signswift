31c259c86fb92f2f3584cb6db66c4dab8329fcdf
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-14 18:12:34 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{ts,tsx}", // App Router pages
-    "./pages/**/*.{ts,tsx}", // Pages Router (if any)
-    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
